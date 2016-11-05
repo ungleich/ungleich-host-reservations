dcc9b871cdc63724b223bd0d01700c4604415ed2
Carlos Ortigoza Dempster <carlos.otgz@gmail.com>
2016-11-05 14:29:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Create me like this:
+
+```bash
+docker run --name hosts --network=ethz-scientific -e DB=kea_db -e USER=kea -e PASSWORD=dummy_password ungleich/ungleich-host-inventory
+```
+
+Later, when you need to know the full list of host reservations, you can call me again like this:
+
+```bash
+docker start -i hosts
+```
+
+When you get bored about me (or if your Postgres credentials change), just delete me:
+
+```bash
+docker rm hosts
+```
---
+++ print-mac-ip-from-kea.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Carlos Ortigoza Dempster (carlos.ortigoza -at- ungleich.ch)
+# 2016-11-05
+
+import os
+import psycopg2
+
+DB = os.environ['DB']
+USER = os.environ['USER']
+PASSWORD = os.environ['PASSWORD']
+
+try:
+    conn = psycopg2.connect("dbname='{}' user='{}' host='postgres' password='{}'".format(DB, USER, PASSWORD))
+except:
+    print("I am unable to connect to the database")
+
+cur = conn.cursor()
+cur.execute("""SELECT encode(dhcp_identifier, 'hex'), ipv4_address, hostname FROM hosts""")
+rows = cur.fetchall()
+
+print("dhcp_identifier\tipv4_address\thostname")
+for row in rows:
+    mac=":".join([row[0][i:i+2] for i in range(0, len(row[0]), 2)])
+    print("{}\t{}\t{}".format(mac, row[1], row[2]))
